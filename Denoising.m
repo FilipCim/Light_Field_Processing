3248c02469a7a97f27a114672978fae5fb9aa225
FilipCim <43687438+FilipCim@users.noreply.github.com>
2018-11-12 13:32:12 +0100

    Denoising script - need to be added into decoding function in LF Toolbox

--- Denoising.m
+++ Denoising.m
@@ -1,13 +1,34 @@
 clear all
 close all
-clc
 
+% Denoising scipt that calculates median of whole image and replaces all
+% pixels with values bigger than treshold * median with median value.
+% Histogram calculated for clarification purposes. 
 %%
 
-testImg = load(
+fileID = '.\data\imagesCamera\Tiff\exps_1_125_iso_1600_image_1.tiff';
+testImg = imread(fileID);
+
+%%
+figure()
+imshow(testImg);
+title('Image with noise');
 
 [vals, edges, bin] = histcounts(testImg);
-thresh = 5; 
-hotPixels = double(testImg).*(testImg>(thresh*medianVal));  
+figure()
+stem(edges(1:1:end-1), vals);
+title('Histogram');
+
+% index = find(vals == max(vals));
+% medianVal = edges(index);
+
+medianVal = median(testImg(:));
+thresh = 5;
+hotPixels = double(testImg).*(testImg > (thresh*medianVal));  
 [row, col] = find(hotPixels); 
-testImg(find(hotPixels))=medianVal;
+testImg = testImg;
+testImg(find(hotPixels)) = medianVal;
+
+figure()
+imshow(testImg);
+title('Image without noise');
